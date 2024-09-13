c81d92d19db2d169ec2daecd09846665baabd930
Garrett York <gbyork18@gmail.com>
2024-09-13 18:24:20 -0500

    texture and player updates
    Disabling v-ram textures, creating consts, and additional player class set up.

--- Scripts/Characters/Player.cs
+++ Scripts/Characters/Player.cs
@@ -1,19 +1,15 @@
 using Godot;
 using System;
-
 public partial class Player : CharacterBody3D
 {
     [ExportGroup("Required Nodes")]
     [Export] private AnimationPlayer animationPlayerNode;
     [Export] private Sprite3D sprite3DNode;
-
     private Vector2 direction = new();
 
     public override void _Ready()
     {
-            GD.Print(animationPlayerNode.Name);
-            GD.Print(sprite3DNode.Name);
-
+        animationPlayerNode.Play(GameConstants.ANIM_IDLE);
     }
     public override void _PhysicsProcess(double delta)
     {
@@ -21,10 +17,33 @@ public partial class Player : CharacterBody3D
         Velocity *= 5;
 
         MoveAndSlide();
+        Flip();
     }
 
     public override void _Input(InputEvent @event)
     {
-        direction = Input.GetVector("Left","Right","Forward","MoveBack");
+        direction = Input.GetVector(
+            GameConstants.ANIM_LEFT,GameConstants.ANIM_RIGHT,
+            GameConstants.ANIM_FORWARD,GameConstants.ANIM_BACK
+        );
+
+        if(direction == Vector2.Zero)
+        {
+            animationPlayerNode.Play(GameConstants.ANIM_IDLE);
+        }
+        else
+        {
+            animationPlayerNode.Play(GameConstants.ANIM_MOVE);
+        }
+    }
+
+    private void Flip()
+    {
+        bool isNotMovingHorizontally = Velocity.X == 0;
+
+        if (isNotMovingHorizontally) {return;}
+
+        bool isMovingLeft = Velocity.X < 0;
+        sprite3DNode.FlipH = isMovingLeft;
     }
 }
---
+++ Scripts/General/GameConstants.cs
@@ -0,0 +1,12 @@
+public class GameConstants
+{
+    public const string ANIM_IDLE = "Idle";
+    public const string ANIM_MOVE = "Move";
+
+    public const string ANIM_LEFT = "Left";
+    public const string ANIM_RIGHT = "Right";
+    public const string ANIM_FORWARD = "Forward";
+    public const string ANIM_BACK = "MoveBack";
+
+
+}
